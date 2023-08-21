9267c5c449e127cc2dfdfdef3bf4cfc57aaf6e62
Ahmed Alaa <ahmedalaahussein00@gmail.com>
2023-08-21 19:07:05 +0300

    add CI/CD configs

--- app/src/main/java/com/ahmed/english_pl/ui/main/EnglishPremierLeagueActivity.kt
+++ app/src/main/java/com/ahmed/english_pl/ui/main/EnglishPremierLeagueActivity.kt
@@ -4,11 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import com.ahmed.english_pl.databinding.ActivityEnglishPremierLeagueBinding
 import com.ahmed.english_pl.ui.base.BaseActivity
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class EnglishPremierLeagueActivity : BaseActivity<ActivityEnglishPremierLeagueBinding>() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
+        AppCenter.start(
+            application, "9107de5f-830a-4780-b26b-64dbab397ea4",
+            Analytics::class.java, Crashes::class.java
+        )
     }
 
     override fun initializeViews() {
